2a6e38b72109d7f285a86f7094dcd7a33a67096d
sakil-sagor <sakilsagor76543@gmail.com>
2024-04-29 11:26:31 +0600

    make some update

--- src/Pages/FrontendPage/ShopPage/ShopPage.jsx
+++ src/Pages/FrontendPage/ShopPage/ShopPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ShopPage = () => {
-  return <div>this is shop page</div>;
+  return (
+    <div>
+      this is shop page
+      <h1>This also a shp page</h1>
+    </div>
+  );
 };
 
 export default ShopPage;
