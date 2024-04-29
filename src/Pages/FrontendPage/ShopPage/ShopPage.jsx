76f7bed891906ce89386fca5c07bfafec5633576
sakil-sagor <sakilsagor76543@gmail.com>
2024-04-30 00:56:39 +0600

    added new thisng

--- src/Pages/FrontendPage/ShopPage/ShopPage.jsx
+++ src/Pages/FrontendPage/ShopPage/ShopPage.jsx
@@ -5,7 +5,11 @@ const ShopPage = () => {
       <p>deleted shp and added new</p>
       <input type="text" />
       <p>this is for only rupak vai</p>
-      =======
+      <div>
+        <from>
+          <p>hello</p>
+        </from>
+      </div>
     </div>
   );
 };
